a2c9f43e160acd12fa1f6c6a5ddab7791b229906
nsr888 <sun604@gmail.com>
2021-04-12 21:54:47 +0300

    Added multiserver support

--- main.cpp
+++ main.cpp
@@ -6,17 +6,26 @@
 #include "Client.hpp"
 #include "Settings.hpp"
 #include "WebServer.hpp"
+#include "EventLoop.hpp"
 
 int main() {
     Settings sample_server_config;
     sample_server_config.port = 7777;
     std::vector<Settings> config;
     config.push_back(sample_server_config);
+    Settings sample_server_config2;
+    sample_server_config2.port = 7778;
+    config.push_back(sample_server_config2);
 
-    for ( std::vector<Settings>::iterator it = config.begin();
-            it != config.end();)
-    {
-        WebServer   server(*it);
-        server.runServer();
+    EventLoop *loop = new EventLoop(config);
+
+    try {
+        loop->initServers();
+        loop->runLoop();
+    } catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        exit(EXIT_FAILURE);
     }
+
+    delete loop;
 }
