e17d522e3a4e24e55514f9d1173735adaad0ab7a
nsr888 <sun604@gmail.com>
2021-04-12 21:43:58 +0300

    echo server responses to stdout

---
+++ tests/echo_server.py
@@ -0,0 +1,15 @@
+'''https://docs.python.org/3/library/http.client.html'''
+import pprint
+import http.client
+import urllib.parse
+
+connection = http.client.HTTPConnection("localhost", port=7777)
+params = urllib.parse.urlencode({'@key1': 'val', '@key2': 'va2'})
+headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
+connection.request("POST", "/echo/post/form", params, headers)
+response = connection.getresponse()
+headers = response.getheaders()
+pp = pprint.PrettyPrinter(indent=4)
+pp.pprint("Headers: {}".format(headers))
+body = response.read()
+pp.pprint("Body: {}".format(body))
