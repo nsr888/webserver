85730a31a35e055af3ee0759bd2164380dce0a57
nsr888 <sun604@gmail.com>
2021-04-13 10:10:49 +0300

    Added POST request test

--- tests/test_server.py
+++ tests/test_server.py
@@ -5,6 +5,7 @@ import pytest
 https://habr.com/ru/company/yandex/blog/242795/
 https://pytest-docs-ru.readthedocs.io/ru/latest/fixture.html
 '''
+import urllib.parse
 
 
 @pytest.fixture
@@ -18,9 +19,22 @@ def connection(request):
     return _connection
 
 
-def test_connection(connection):
-    '''Test responses'''
+def test_get_request(connection):
+    '''Test get request'''
     connection.request("GET", "/")
+    resp = connection.getresponse()
+    assert "Hello world!" in resp.read().decode('utf-8')
+    assert resp.getheader('Content-Type') == 'text/plain'
+    assert resp.version == 11
+    assert resp.status == 200
+    assert resp.reason == 'OK'
+
+
+def test_post_request(connection):
+    '''Test post request'''
+    params = urllib.parse.urlencode({'@key1': 'val', '@key2': 'va2'})
+    headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
+    connection.request("POST", "/", params, headers)
     resp = connection.getresponse()
     assert "Hello world!" in resp.read().decode('utf-8')
     assert resp.getheader('Content-Type') == 'text/plain'
