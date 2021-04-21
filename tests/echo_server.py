de10abd069aa7341a16df3d19772a2fb4e2e61a4
nsr888 <sun604@gmail.com>
2021-04-21 07:49:15 +0300

    Added test for large file

--- tests/echo_server.py
+++ tests/echo_server.py
@@ -4,6 +4,7 @@ import http.client
 import urllib.parse
 
 connection = http.client.HTTPConnection("localhost", port=7777)
+
 params = urllib.parse.urlencode({'@key1': 'value1', '@key2': 'value2!!!!'})
 headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
 connection.request("POST", "/echo/post/form", params, headers)
@@ -13,3 +14,12 @@ pp = pprint.PrettyPrinter(indent=4)
 pp.pprint("Headers: {}".format(headers))
 body = response.read()
 pp.pprint("Body: {}".format(body))
+
+headers2 = {"Content-type": "text/html"}
+connection.request("GET", "/files/test_large_file.html", "", headers2)
+response = connection.getresponse()
+headers = response.getheaders()
+pp = pprint.PrettyPrinter(indent=4)
+pp.pprint("Headers: {}".format(headers))
+body = response.read()
+# pp.pprint("Body: {}".format(body))
