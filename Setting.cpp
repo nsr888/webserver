0b8d028282acd32e92bff4922440e35d86657bef
Igor <resther@student.21-school.ru>
2021-04-22 14:29:04 +0500

    test fill config added
    test fill config added

--- Setting.cpp
+++ Setting.cpp
@@ -1,6 +1,6 @@
 #include "Setting.hpp"
 
-void Setting::setHost(std::string& host) {
+void Setting::setHost(std::string host) {
 	_host = host;
 }
 
@@ -8,10 +8,55 @@ void Setting::setPort(int port) {
 	_port = port;
 }
 
-void Setting::setServerName(std::string& server_name) {
+void Setting::setServerName(std::string server_name) {
 	_server_name = server_name;
 }
 
+<<<<<<< Updated upstream
 void Setting::setErrorPage(std::string& error_page) {
 	_errorPage = error_page;
 }
+=======
+
+void Setting::testfillError(void) {
+	_errorLocation.push_back("Users");
+	_errorLocation.push_back("anasyrov");
+	_errorLocation.push_back("Documents");
+	_errorLocation.push_back("21");
+	_errorLocation.push_back("webserver");
+	_errorLocation.push_back("webserver");
+	_errorLocation.push_back("files");
+	_errorLocation.push_back("error_page.html");
+}
+
+void Setting::testfillIndex(void) {
+	_indexLocation.push_back("Users");
+	_indexLocation.push_back("anasyrov");
+	_indexLocation.push_back("Documents");
+	_indexLocation.push_back("21");
+	_indexLocation.push_back("webserver");
+	_indexLocation.push_back("webserver");
+	_indexLocation.push_back("files");
+	_indexLocation.push_back("index.html");
+}
+
+void Setting::testfillFavicon(void) {
+	_faviconLocation.push_back("Users");
+	_faviconLocation.push_back("anasyrov");
+	_faviconLocation.push_back("Documents");
+	_faviconLocation.push_back("21");
+	_faviconLocation.push_back("webserver");
+	_faviconLocation.push_back("webserver");
+	_faviconLocation.push_back("files");
+	_faviconLocation.push_back("favicon.ico");
+}
+
+void Setting::testfillRoot(void) {
+	_rootLocation.push_back("Users");
+	_rootLocation.push_back("anasyrov");
+	_rootLocation.push_back("Documents");
+	_rootLocation.push_back("21");
+	_rootLocation.push_back("webserver");
+	_rootLocation.push_back("webserver");
+}
+>>>>>>> Stashed changes
--- Setting.hpp
+++ Setting.hpp
@@ -1,29 +1,48 @@
 #ifndef SETTING_HPP
 # define SETTING_HPP
 # include <string>
+# include <vector>
 
 class Setting {
 private:
-    std::string     _server_name;
-    std::string     _host;
-    std::string     _errorPage;
-    int             _port;
+    std::string                 _server_name;
+    std::string                 _host;
+    int                         _port;
+    std::vector<std::string>    _rootLocation;
+    std::vector<std::string>    _errorLocation;
+    std::vector<std::string>    _indexLocation;
+    std::vector<std::string>    _faviconLocation;
+    std::vector<std::string>    _rootLocation;
 
 public:
-    Setting() {};
+    Setting(void) {
+        _server_name;
+        _host;
+        _port;
+        _rootLocation;
+        _errorLocation;
+        _indexLocation;
+        _faviconLocation;
+        _rootLocation;
+    };
+
     ~Setting() {};
 
     /* Setters */
-    void			setHost(std::string& host);
+    void			setHost(std::string host);
 	void			setPort(int port);
-	void			setServerName(std::string& server_name);
-	void			setErrorPage(std::string& error_page);
+	void			setServerName(std::string server_name);
+
+    /* for test only */
+    void Setting::testfillError(void);
+    void Setting::testfillIndex(void);
+    void Setting::testfillFavicon(void);
+    void Setting::testfillRoot(void);
 
     /* Getters */
 	std::string& 		getHost() { return _host; }
 	int	                getPort() { return _port; }
 	std::string& 		getServerName() { return _server_name; }
-	std::string&		getErrorPage() { return _errorPage; }
 };
 
 #endif
