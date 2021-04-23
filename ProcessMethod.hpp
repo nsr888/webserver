9f435cc53d01cc1bebe7751c84003815567816b7
kozarezov <kozarezov.dd@gmail.com>
2021-04-23 16:50:49 +0300

    Process

--- ProcessMethod.hpp
+++ ProcessMethod.hpp
@@ -18,7 +18,7 @@ public:
 
 	void			secretary_Request(); /* Функция секретарь для отправки в нужный метод */
 	void			generateBody(); /* функция должна читать по пути и в итоге записать это в Response::setBody(std::string) и Response::setBodySize(int) */
-
+	
 	void			processGetRequest();
 	void			processHeadRequest();
 	void			processPostRequest();
