fd609f6f65e3b27c29d2ae7e4cc4703e7e27ade3
nsr888 <sun604@gmail.com>
2021-04-28 07:57:47 +0300

    Fix write_file_raw (added O_TRUNC), added file_exists function

--- utils.cpp
+++ utils.cpp
@@ -1,4 +1,5 @@
 #include <unistd.h>
+#include <sys/stat.h>
 #include "utils.hpp"
 
 namespace utils {
@@ -41,7 +42,7 @@ namespace utils {
 
         offset = 0;
         m = 0;
-        if ((fd = open(filename.c_str(), O_WRONLY|O_CREAT)) < 0)
+        if ((fd = open(filename.c_str(), O_TRUNC | O_WRONLY | O_CREAT)) < 0)
             throw std::runtime_error(std::string("open: ") + strerror(errno));
         std::vector<char>::iterator it = buf.begin();
         while (it != buf.end())
@@ -56,6 +57,11 @@ namespace utils {
         if (m == -1)
             throw std::runtime_error(std::string("write: ") + strerror(errno));
         close(fd);
+    }
+
+    bool file_exists (std::string filename) {
+      struct stat   buffer;   
+      return (stat (filename.c_str(), &buffer) == 0);
     }
 }
 
