5efd39c25b61434133bbc89f27fa068ffdc56737
nsr888 <sun604@gmail.com>
2021-05-11 10:21:33 +0300

    fix: Minor changes

--- files/test.php
+++ files/test.php
@@ -1,3 +1,13 @@
 <?php
-phpinfo();
+if (!empty($_POST["key1"])) {
+    echo "<html><body><pre>\n";
+    echo "Key 'key1' is set in POST request to value '".$_POST["key1"]."'\n";
+    echo "POST array values:\n";
+    print_r($_POST);
+} else {  
+    echo "<html><body><pre>\n";
+    echo "Key 'key1' is NOT set in POST request\n";
+    echo "POST array values:\n";
+    print_r($_POST);
+}
 ?>
