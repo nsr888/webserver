425ead827303f1dd2e884201bd62bce7a369970d
nsr888 <sun604@gmail.com>
2021-05-22 11:55:23 +0300

    Add html tags

--- files/test.php
+++ files/test.php
@@ -1,13 +1,13 @@
 <?php
 if (!empty($_POST["key1"])) {
-    echo "<html><body><pre>\n";
-    echo "Key 'key1' is set in POST request to value '".$_POST["key1"]."'\n";
-    echo "POST array values:\n";
+    echo "<html><body>";
+    echo "<h2>Key 'key1' is set in POST request to value '".$_POST["key1"]."'</h2>";
+    echo "POST array values:<br/><pre>";
     print_r($_POST);
 } else {  
-    echo "<html><body><pre>\n";
-    echo "Key 'key1' is NOT set in POST request\n";
-    echo "POST array values:\n";
+    echo "<html><body>";
+    echo "<h2>Key 'key1' is NOT set in POST request</h2>";
+    echo "POST array values:<br/><pre>";
     print_r($_POST);
 }
 ?>
